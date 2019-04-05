1f9038b82976dac645a421cecf6e064677a0fb5c
Ines Guerrero <inesgs@gmail.com>
2019-04-05 12:23:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function takeANumber(katzDeliLine, name) {
+function takeANumber(katzDeliLine) {
   katzDeliLine.push(name)
   return ("Welcome, " + name + ". You are number " + (katzDeliLine.length) + " in line.")
 }
